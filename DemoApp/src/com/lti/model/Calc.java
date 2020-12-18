9d58f9df1b0fd44367bb69f2bd900e051975e844
MohanNaik01 <76149011+MohanNaik01@users.noreply.github.com>
2020-12-18 13:06:47 +0530

    New Method Added

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -6,6 +6,9 @@ public class Calc {
 		System.out.println("Sum= "+(num1+num2));
 	}
 	public void diff(int num1,int num2) {
-		System.out.println("Sum= "+(num1-num2));
+		System.out.println("Diff= "+(num1-num2));
+	}
+	public void multi(int num1,int num2) {
+		System.out.println("Multi= "+(num1*num2));
 	}
 }
